37d1740da5e1360fbf15ed7868c253e87185cf24
RyanSyen <ryanwong710200@gmail.com>
2022-09-15 06:54:22 +0800

    Add existing project files to repo

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ctvfjmlxrote.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ryanwongyisyen-bucket"
+customregion = "us-east-1"
